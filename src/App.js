d308233f759c5270961ef15bd2a61fbf7138f770
Artufelii <artufelii@gmail.com>
2021-03-02 11:44:33 -0600

    Se cambia parte de dashboard a parte independiente por cuentiones de practicidad

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Switch, Route, useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
 import axios from './components/axios'
@@ -12,11 +12,8 @@ import Servicios from './components/Servicios'
 import Blog from './components/Blog'
 import Article from './components/Article'
 import Property from './components/Property'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
 
 function App() {
-  const history = useHistory()
   const [succeeded, setSucceeded] = useState(false)
   const [processing, setProcessing] = useState(false)
   const [client, setClient] = useState("")
@@ -48,70 +45,43 @@ function App() {
 
   }
 
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      if (window.location.pathname === '/login') {
-        history.push('/dashboard/desk')
-      }
-    } else {
-      if(/\/dashboard\/./.test(window.location.pathname)) {
-        history.push('/login')
-      }
-    }
-  }, [history])
-
   return (
       <div className="App">
+        <Header />
         <Switch>
-          <Route exact path="/dashboard/desk" component={Dashboard} />
-          <Route exact path="/login" component={Login}/>
           <Route path="/propiedades/:slug">
-            <Header />
             <Property />
-            <Footer />
           </Route>
           <Route path="/blog/:slug">
-            <Header />
             <Article />
-            <Footer />
           </Route>
           <Route path="/blog">
-            <Header />
             <Blog />
-            <Footer />
           </Route>
           <Route path="/propiedades">
-            <Header />
             <Propiedades />
-            <Footer />
           </Route>
           <Route path="/servicios">
-            <Header />
             <Servicios />
-            <Footer />
           </Route>
           <Route path="/contacto">
-            <Header />
             <Contacto 
               handleSubmit={ handleSubmit }
               client= { client }
               processing= { processing }
               succeeded= { succeeded }
             />
-            <Footer />
           </Route>
           <Route exact path="/">
-            <Header />
             <Home
               handleSubmit={ handleSubmit }
               client= { client }
               processing= { processing }
               succeeded= { succeeded }
             />
-            <Footer />
           </Route>
         </Switch>
+        <Footer />
       </div>
   );
 }
