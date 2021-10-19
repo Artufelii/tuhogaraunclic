1d5dd55e66f5d6cbb61e1b00b214579663d8553a
Artufelii <artufelii@gmail.com>
2021-10-19 13:34:43 -0500

    commit par poner al corriente las ramas

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-let url
+const url = 'https://tuhogaraunclic.herokuapp.com/'
 
-if (process.env.NODE_ENV === 'production') {
-  url = 'https://tuhogaraunclic.herokuapp.com/' 
-} else {
-  url = 'http://localhost:4000'
-}
+//if (process.env.NODE_ENV === 'production') {
+  //url = 'https://tuhogaraunclic.herokuapp.com/' 
+//} else {
+  //url = 'http://localhost:4000'
+//}
 
 const instance = axios.create({
   baseURL: url
