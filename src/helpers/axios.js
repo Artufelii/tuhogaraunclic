e6529fa14a62ff9ee9dab9262c4467debac2f011
Artufelii <artufelii@gmail.com>
2021-10-20 01:15:24 -0500

    commit para arreglar crasheo

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,11 +2,6 @@ import axios from 'axios'
 
 const url = 'https://tuhogaraunclic.herokuapp.com/'
 
-//if (process.env.NODE_ENV === 'production') {
-  //url = 'https://tuhogaraunclic.herokuapp.com/' 
-//} else {
-  //url = 'http://localhost:4000'
-//}
 
 const instance = axios.create({
   baseURL: url
