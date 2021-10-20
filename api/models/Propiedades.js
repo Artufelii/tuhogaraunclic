deca7160d600881e0f1f79d4b514b84e31706084
Artufelii <artufelii@gmail.com>
2021-10-20 01:23:55 -0500

    Se soluciona problema de crasheo

--- api/models/Propiedades.js
+++ api/models/Propiedades.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const mongoose  = require('mongoose');
-const Schema = mongoose.Schema
-
-const Propiedades = mongoose.model('Propiedad', new Schema({
-	title: String,
-	description: String,
-	adress: String,
-	price: String,
-	chars: {
-		land: String,
-		construction: String,
-		bedrooms: String,
-		restrooms: String,
-		parking: String,
-	},
-	images: {
-		cover: String,
-		image1: String,
-		image2: String,
-		image3: String,
-		image4: String,
-		image5: String,
-		image6: String,
-		image7: String,
-		image8: String,
-		image9: String,
-	},
-	slug: String,
-}))
-
-module.exports = Propiedades
-=======
 const mongoose  = require('mongoose');
 const Schema = mongoose.Schema
 
@@ -64,4 +31,3 @@ const Propiedades = mongoose.model('Propiedad', new Schema({
 }))
 
 module.exports = Propiedades
->>>>>>> 376c4f71516a2a1f0ee2396c70be1de3b576b446
